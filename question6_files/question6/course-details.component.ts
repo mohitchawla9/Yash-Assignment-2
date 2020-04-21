2b8a9a40e2b3f9c72485f4923a5e65f5fb655a6b
mohitchawla9 <63363623+mohitchawla9@users.noreply.github.com>
2020-04-21 17:12:24 +0530

    Assignment 6 Question 6

---
+++ question6_files/question6/course-details.component.ts
@@ -0,0 +1,23 @@
+import { Component , OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+
+@Component({
+  selector: 'app-course-details',
+  template: `<h1>You have selected course-id = {{ id }}`,
+})
+export class CourseDetailsComponent implements OnInit {
+  id: number;
+  private sub: any;
+
+  constructor(private route: ActivatedRoute) {}
+
+  ngOnInit() {
+    this.sub = this.route.params.subscribe(params => {
+       this.id = +params['id']; // (+) converts string 'id' to a number
+
+       // In a real app: dispatch action to load the details here.
+    });
+
+  } 
+}
+
