8c323e18661c043748acfb6834dd8d2cff4eddf9
mohitchawla9 <63363623+mohitchawla9@users.noreply.github.com>
2020-04-21 17:15:03 +0530

    Assignment 2 Question 7

---
+++ question7_files/src/app/root/root.component.ts
@@ -0,0 +1,36 @@
+import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+
+@Component({
+  selector: 'app-registration-from',
+  templateUrl: './root.component.html',
+})
+export class RootComponent implements OnInit {
+  registerForm : FormGroup;
+  submitted = false;
+  constructor(private formBuilder: FormBuilder) { }
+
+  ngOnInit() {
+    this.registerForm = this.formBuilder.group({
+            firstName: ['', Validators.required],
+            lastName: ['', Validators.required],
+            email: ['', [Validators.required, Validators.email]],
+            password: ['', [Validators.required, Validators.minLength(6)]],
+            confirmPassword: ['', Validators.required]
+    }, {
+      
+    })
+  }
+  // convenience getter for easy access to form fields
+  get f() { return this.registerForm.controls; }
+
+  onSubmit() {
+      this.submitted = true;
+
+      // stop here if form is invalid
+      if (this.registerForm.invalid) {
+          return;
+      }
+      alert('SUCCESS!! :-)\n\n' + JSON.stringify(this.registerForm.value))
+  }
+}
